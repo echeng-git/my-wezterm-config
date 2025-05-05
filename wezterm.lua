0543648f9abaab63b4c53194ea8f5f8119c1dfad
Eric <90707245+echeng-git@users.noreply.github.com>
2025-05-05 13:54:52 -0600

    <C+BS> shortcut because I am too used to it

--- wezterm.lua
+++ wezterm.lua
@@ -3,7 +3,6 @@ local act = wezterm.action
 local config = wezterm.config_builder()
 
 -- store the actual configuration
-
 config.font = wezterm.font("D2CodingLigature Nerd Font")
 config.font_size = 12.0
 config.window_background_opacity = 0.7
@@ -13,12 +12,21 @@ config.win32_system_backdrop = "Acrylic"
 -- keybinding
 config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 3000 }
 config.keys = {
+
+	-- bind <C+BS> to delete previous word
+	{
+		key = "Backspace",
+		mods = "CTRL",
+		action = act.SendString("\x1b\x7f"),
+	},
+
 	-- tab navigation
 	{
 		key = "H",
 		mods = "CTRL|SHIFT",
 		action = act.ActivateTabRelative(-1), -- Move to previous tab :contentReference[oaicite:2]{index=2}
 	},
+
 	{
 		key = "L",
 		mods = "CTRL|SHIFT",
@@ -31,6 +39,7 @@ config.keys = {
 		mods = "CTRL|SHIFT",
 		action = act.CopyTo("Clipboard"), -- Copy selection to system clipboard :contentReference[oaicite:4]{index=4}
 	},
+
 	{
 		key = "V",
 		mods = "CTRL|SHIFT",
@@ -43,17 +52,19 @@ config.keys = {
 		mods = "LEADER|SHIFT",
 		action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
 	},
+
 	{
 		key = "-",
 		mods = "LEADER",
 		action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
 	},
+
 	{
 		key = "d",
 		mods = "LEADER",
-
 		action = act.CloseCurrentPane({ confirm = true }),
 	},
+
 	{
 		key = "h",
 		mods = "LEADER",
@@ -65,20 +76,20 @@ config.keys = {
 		mods = "LEADER",
 		action = act.ActivatePaneDirection("Down"),
 	},
+
 	{
 		key = "k",
 		mods = "LEADER",
 		action = act.ActivatePaneDirection("Up"),
 	},
+
 	{
 		key = "l",
 		mods = "LEADER",
-
 		action = act.ActivatePaneDirection("Right"),
 	},
 
-	-- Tab manipulation
-
+	-- tab create/delete
 	{ key = "t", mods = "LEADER", action = act.SpawnTab("CurrentPaneDomain") },
 	{ key = "T", mods = "LEADER", action = act.CloseCurrentTab({ confirm = true }) },
 }
@@ -86,7 +97,6 @@ config.keys = {
 -- tab index mapping
 for i = 1, 9 do
 	table.insert(config.keys, {
-
 		key = tostring(i),
 		mods = "LEADER",
 		action = act.ActivateTab(i - 1),
